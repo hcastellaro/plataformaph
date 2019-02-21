6fed27c515cbb582393e2bbf82134e8c99bbc838
hcastellaro <castellaro@gmail.com>
2019-02-20 23:50:22 -0300

    Aplicaciones de mapas

---
+++ casos/casos.js
@@ -0,0 +1,142 @@
+var capa_consultorios = {
+    "type": "FeatureCollection",
+    "name": "consultorios",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "Los Milagros" }, "geometry": { "type": "Point", "coordinates": [ -58.64520929, -34.61901633 ] } },
+    { "type": "Feature", "properties": { "Name": "Patagones" }, "geometry": { "type": "Point", "coordinates": [ -58.63046198, -34.68232016 ] } },
+    { "type": "Feature", "properties": { "Name": "Santa Rosa" }, "geometry": { "type": "Point", "coordinates": [ -58.55207311, -34.45981615 ] } },
+    { "type": "Feature", "properties": { "Name": "Un Techo Para Todos" }, "geometry": { "type": "Point", "coordinates": [ -58.55756269, -34.72385811 ] } },
+    { "type": "Feature", "properties": { "Name": "Maria Elena" }, "geometry": { "type": "Point", "coordinates": [ -58.61794788, -34.73735697 ] } }
+    ]
+    };
+
+var capa_gestion_procesos_urbanizacion = {
+    "type": "FeatureCollection",
+    "name": "gestion_procesos_urbanizacion",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "San Fernando Oeste" }, "geometry": { "type": "Point", "coordinates": [ -58.60017245, -34.47174641 ] } },
+    { "type": "Feature", "properties": { "Name": "Santa Rosa" }, "geometry": { "type": "Point", "coordinates": [ -58.55207311, -34.45981615 ] } },
+    { "type": "Feature", "properties": { "Name": "Gardel | Pte. Sarmiento" }, "geometry": { "type": "Point", "coordinates": [ -58.57802667, -34.62495494 ] } },
+    { "type": "Feature", "properties": { "Name": "San Ignacio" }, "geometry": { "type": "Point", "coordinates": [ -58.50213056, -34.82108679 ] } },
+    { "type": "Feature", "properties": { "Name": "La Carbonilla" }, "geometry": { "type": "Point", "coordinates": [ -58.46951218, -34.59837605 ] } },
+    { "type": "Feature", "properties": { "Name": "Los Pinos" }, "geometry": { "type": "Point", "coordinates": [ -58.45092314, -34.65672751 ] } }
+    ]
+    };
+
+var capa_ph_casos_estudio = {
+    "type": "FeatureCollection",
+    "name": "ph_casos_estudio",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "Villa Palito", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.54826505, -34.69765029 ] } },
+    { "type": "Feature", "properties": { "Name": "Un Techo Para Todos", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.54786115, -34.72236281 ] } },
+    { "type": "Feature", "properties": { "Name": "Carlos Gardel - Pte. Sarmiento", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.57806819, -34.62435002 ] } },
+    { "type": "Feature", "properties": { "Name": "San Fernando Oeste", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.59741934, -34.46650928 ] } },
+    { "type": "Feature", "properties": { "Name": "Villa Tranquila", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.35565962, -34.65088686 ] } },
+    { "type": "Feature", "properties": { "Name": "Monte Matadero", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.25614629, -34.71113615 ] } },
+    { "type": "Feature", "properties": { "Name": "Patagones", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.63067404, -34.68263012 ] } }
+    ]
+    };
+
+var capa_ph_coopteba = {
+    "type": "FeatureCollection",
+    "name": "ph_coopteba",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "Coopteba SM\/3F | 18 Viviendas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.56163646, -34.56023588 ] } },
+    { "type": "Feature", "properties": { "Name": "Coopteba SM\/3F | 6 Viviendas Transitorias", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.5546969, -34.58896558 ] } },
+    { "type": "Feature", "properties": { "Name": "Coopteba VL | 11 Viviendas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.55956136, -34.44599304 ] } }
+    ]
+    };
+
+var capa_ph_cetep = {
+    "type": "FeatureCollection",
+    "name": "ph_ctep",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "PF Campana | 104 Viviendas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.9496679, -34.19447887 ] } },
+    { "type": "Feature", "properties": { "Name": "PF San Martín | 200 Viveindas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.58639032, -34.51719041 ] } },
+    { "type": "Feature", "properties": { "Name": "PF Villanueva | 200 Viviendas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.77368976, -34.57880032 ] } },
+    { "type": "Feature", "properties": { "Name": "PF Moreno | 246 Viviendas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.79816567, -34.61400703 ] } },
+    { "type": "Feature", "properties": { "Name": "PF Escobar | 122 Viviendas", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.78055686, -34.39377074 ] } }
+    ]
+    };
+
+var capa_jardines_hurlingham = {
+    "type": "FeatureCollection",
+    "name": "ph_jardines_hurlingham",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "Jardin de Infantes N° 2 Sendero de Luna", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.6449889, -34.6025093, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "Jardín Maternal N° 5 Rayito de Sol", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.650046, -34.6204972, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "Jardín de Infantes N° 918", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.6638218, -34.5883331, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "Jardín de Infantes N° 919", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.6518431, -34.5714303, 0.0 ] } }
+    ]
+    };
+
+var capa_jardines_moreno = {
+    "type": "FeatureCollection",
+    "name": "ph_jardines_moreno",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "CTA | Jardin N° 906", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.85617442, -34.63516254, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "CTA | Jardin N° 926", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.8258534, -34.63682949, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "CTA | Jardin N° 955", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.81515154, -34.53883356, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "CTA | Jardin N° 930", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.79052582, -34.6093677, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "CTA | Jardin N° 918", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.76236642, -34.59968473, 0.0 ] } }
+    ]
+    };
+
+var capa_ph_proyectos = {
+    "type": "FeatureCollection",
+    "name": "ph_proyectos",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "01- Asociación Casa de Todos", "description": "2013 - Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.6404763, -34.7635857 ] } },
+    { "type": "Feature", "properties": { "Name": "02- Casa del niño Rucalhue", "description": "2014 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.1898084, -34.7869617 ] } },
+    { "type": "Feature", "properties": { "Name": "03- Comedor Alegres en Jesus", "description": "2012 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.5763539, -34.6136291 ] } },
+    { "type": "Feature", "properties": { "Name": "04- Comedor Exaltacion de la Cruz", "description": "2013 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.31615277, -34.74662117 ] } },
+    { "type": "Feature", "properties": { "Name": "05- Comedor Maria del Rosario", "description": "2013 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.33292076, -34.74919847 ] } },
+    { "type": "Feature", "properties": { "Name": "06 - Medalla Milagrosa", "description": "2013 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.3199176, -34.75003143 ] } },
+    { "type": "Feature", "properties": { "Name": "07- Comedor Ntra. Señora de Guadalupe", "description": "2014-2015 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.3210565, -34.7595719 ] } },
+    { "type": "Feature", "properties": { "Name": "08- Comedor Ntra. Señora de Itati", "description": "2014-2015 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.3003181, -34.7978055 ] } },
+    { "type": "Feature", "properties": { "Name": "09- Comedor Ntra. Señora del Rosario", "description": "2014 - 2015 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.2994679, -34.7764028 ] } },
+    { "type": "Feature", "properties": { "Name": "10- Comedor Ntra. Señora de Lourdes", "description": "2010 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.2931888, -34.8148603 ] } },
+    { "type": "Feature", "properties": { "Name": "11- Club Yupanqui", "description": "2014  - Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.48158231, -34.68385804 ] } },
+    { "type": "Feature", "properties": { "Name": "12- Jardin Villa del Parque", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.483466, -34.60402744 ] } },
+    { "type": "Feature", "properties": { "Name": "13- Sede \"La Territorial\"", "description": "2015 - Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.3868998, -34.5810791 ] } },
+    { "type": "Feature", "properties": { "Name": "14- Una Mano que ayuda a la otra", "description": "2014 - Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.2269329, -34.8177715 ] } },
+    { "type": "Feature", "properties": { "Name": "15 - Mártires Rioplatenses", "description": "Proyecto 2017 - Caritas" }, "geometry": { "type": "Point", "coordinates": [ -58.28854164, -34.82573777 ] } },
+    { "type": "Feature", "properties": { "Name": "16 - Guadalupe", "description": "Proyecto 2017 - Caritas" }, "geometry": { "type": "Point", "coordinates": [ -58.31724099, -34.75720424 ] } },
+    { "type": "Feature", "properties": { "Name": "17 - Exaltación de la Cruz", "description": "Proyecto 2017 - Caritas" }, "geometry": { "type": "Point", "coordinates": [ -58.3222137, -34.75246099 ] } },
+    { "type": "Feature", "properties": { "Name": "18 - Santa Teresa", "description": "Proyecto 2017" }, "geometry": { "type": "Point", "coordinates": [ -58.33065484, -34.76080204 ] } },
+    { "type": "Feature", "properties": { "Name": "19 AVEFA | Polideportivo", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.42586066, -34.62235355 ] } },
+    { "type": "Feature", "properties": { "Name": "Polideportivo Municipal EE", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.51003582, -34.80589343 ] } },
+    { "type": "Feature", "properties": { "Name": "Lujan | Polideportivo", "description": null }, "geometry": { "type": "Point", "coordinates": [ -59.12459102, -34.57106371 ] } },
+    { "type": "Feature", "properties": { "Name": "Moreno | Ctro Cultural Chavez", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.79341444, -34.65003791 ] } },
+    { "type": "Feature", "properties": { "Name": "Moreno, Terciario", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.81230226, -34.54400412 ] } },
+    { "type": "Feature", "properties": { "Name": "Plaza San Ignacio", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.5023634, -34.82261919 ] } },
+    { "type": "Feature", "properties": { "Name": "Parque de las victorias - Lugano", "description": null }, "geometry": { "type": "Point", "coordinates": [ -58.46127208, -34.68217047 ] } }
+    ]
+    };
+
+var capa_ph_proyectos_obras = {
+    "type": "FeatureCollection",
+    "name": "ph_proyectos_obras",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "Name": "18- Comedor La Ranita Feliz", "description": "2011 - Proyecto y Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.7389804, -34.5782956, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "19- Comedor Nueva Ana", "description": "2010 Proyecto | Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.3312986, -34.6832625, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "20- Comedor Santa Teresita", "description": "2010 Proyecto" }, "geometry": { "type": "Point", "coordinates": [ -58.3077422, -34.6857064, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "21- Comedor Sociedad de Fomento Esperanza Grande", "description": "2012-2013 Proyecto | 2014-2015 Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.2557901, -34.7642124, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "22- Jardín maternal El arca de los niños", "description": "2013 Proyecto | 2014 Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.2478449, -34.7263085, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "23- Sala de Salud - María Elena", "description": "2011 - Proyecto y Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.6168255, -34.7352833, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "25- Niño Dios", "description": "2011 Proyecto y Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.2328026, -34.8245275, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "26- Centro Misionero Padre Pere Riera", "description": "2014 Proyecto | 2014-2015 Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.5483634, -34.7203495, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "27- Comedor El colectivo de la Esperanza", "description": "Proyecto 2014 Obra 2015-2016" }, "geometry": { "type": "Point", "coordinates": [ -58.3045758, -34.8174619, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "28- Comedor El Sol", "description": "2008 Proyecto | 2010 Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.64524349, -34.75483274, 0.0 ] } },
+    { "type": "Feature", "properties": { "Name": "31- Jardín maternal Los Angelitos", "description": "2014 Obra" }, "geometry": { "type": "Point", "coordinates": [ -58.2577042, -34.72779, 0.0 ] } }
+    ]
+    };
